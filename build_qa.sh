f12f498c0e8fa24fe1cb2a9f659ab4769bde25cd
mickey-ng <mickey.ng@uhn.ca>
2023-12-08 16:12:07 -0500

    add build script

---
+++ build_qa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# tells the shell to mark variables for export,
+# which means they will be available to child processes.
+set -a
+#sources the environment file,
+# effectively loading the environment variables into the current shell.
+. /etc/environment
+# unsets the export flag.
+set +a
+SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+uuid=$(uuidgen)
+
+echo $SELFDIR
+
+TECHNA_HOSTNAME=qa02.technainstitute.net
+TECHNA_ROLE=qa
+TECHNA_REGISTRY_PORT=443
+TECHNA_REGISTRY_ENDPOINT=docker-registry.uhn.ca
+
+MM_API_DB_CONTAINER_IMAGE_NAME=matchminer-api-qa
+MM_API_DB_CONTAINER_IMAGE_LOCATION=$TECHNA_REGISTRY_ENDPOINT:$TECHNA_REGISTRY_PORT/$MM_API_DB_CONTAINER_IMAGE_NAME
+
+docker build -t ${MM_API_DB_CONTAINER_IMAGE_LOCATION}:$uuid $SELFDIR
