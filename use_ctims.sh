a592a0f7970039fbfa6dedd89f9da60233041c3f
mickey-ng <mickey.ng@uhn.ca>
2025-04-10 10:43:51 -0400

    add script to switch matchminer config

---
+++ use_ctims.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp secrets_json_dev_ctims.json secrets_json_dev.json
+echo "Switched to CTIMS config."
---
+++ use_pmatch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp secrets_json_dev_pmatch.json secrets_json_dev.json
+echo "Switched to PMATCH config."
