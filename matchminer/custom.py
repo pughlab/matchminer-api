a70e72fe863950cac1d6b12b92ca053d9d1a0016
mickey-ng <mickey.ng@uhn.ca>
2025-09-15 15:40:50 -0400

    fix mismatch code on delete trial

--- matchminer/custom.py
+++ matchminer/custom.py
@@ -629,11 +629,15 @@ def get_trial_by_protocol():
 
     return resp
 
-@blueprint.route('/api/delete_trial_by_internal_id', methods=['DELETE'])
+@blueprint.route('/api/delete_trial_by_internal_id', methods=['POST'])
 @nocache
 @auth_required
 def delete_trial_by_internal_id():
-    trial_internal_id = request.args.get("trial_internal_id")
+    request_data = request.get_json()
+    trial_internal_id = None
+    
+    if request_data and 'params' in request_data:
+        trial_internal_id = request_data['params'].get('trial_internal_id')
 
     if trial_internal_id is not None:
         database.get_collection('trial').delete_many({"trial_internal_id": trial_internal_id})
